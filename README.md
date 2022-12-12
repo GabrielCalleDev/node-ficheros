7fd5b4395428e8d6e73a765d8fe2495606b5a3d0
Gabriel Samir Calle Torrez <a222603gc@correu.escoladeltreball.org>
2022-12-12 16:39:28 +0100

    Entorno ficheros preparado

---
+++ README.md
@@ -0,0 +1,16 @@
+# Pt1.4 - Practicas Fitxers
+Crear una web con dos páginas:
+
+1. Subir libro (/books/new
+
+Pagina con un formulario que subira a la base de datos la información de un nuevo libro:
+
+    - Nombre
+    - Descripcion
+    - Editorial
+    - Archivo PDF del libro
+    - Ademas se guardará el timestamp de cuando se subió el libro
+
+2. Lista de libros (/books/list)
+
+Lista de los libros (tabla) con la información de cada libro junto con el enlace al PDF para descargar. Tambíen aparecerá la fecha en la que se subio cada libro con el formato dd/mm/aaaa
---
+++ app/app.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const port    = 3000
+const express = require("express"),
+  mongoose = require('mongoose'),
+  app      = express(),
+  server   = require("http").createServer(app),
+  path     = require("path")
+
+// Server starting at port 3000
+server.listen(port, (err, res) => {
+  if (err) console.log(`ERROR: Connecting APP ${err}`)
+  else console.log(`Server is running on port ${port}`)
+});
+
+// Connection to MongoDB
+mongoose.connect(
+  `mongodb://root:pass12345@mongodb:27017/ficheros?authSource=admin`,
+  { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true },
+  (err, res) => {
+    if (err) console.log(`ERROR: connecting to Database.  ${err}`)
+    else console.log(`Database Online`)
+  }
+);
+
+// view engine setup and other configurations
+app.set("views", path.join(__dirname,"views"))
+app.set("view engine", "pug")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
+
+// Import routes of our app
+const routes = require("./routes/main")
+// mount the routes on the app
+app.use("/", routes)
+
+module.exports = app
